8f0aaeb5fa8f3ba701cf2eb4bf1063fbb84f8c80
Solomon ADAMS <solo24@live.com>
2025-09-15 17:19:47 +0200

    correction image

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -194,7 +194,7 @@ export default function Home() {
             <div className="sj-card">
               <h3 className="mb-8">Avant / Après</h3>
               <div className="sj-placeholder">
-                <img src={img7} alt="Avant après Calma Bonita" />
+                <img className="sj-img-fluide" src={img7} alt="Avant après Calma Bonita" />
               </div>
             </div>
           </div>
